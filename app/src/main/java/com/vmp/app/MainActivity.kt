b7a81dd57dc28dfd9d0a343bf1c7c954d7f7233c
philipsB36U <philipsph357@gmail.com>
2026-02-27 04:36:53 +0700

    Create MainActivity.kt

---
+++ app/src/main/java/com/vmp/app/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.vmp.app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialTheme {
+                Surface {
+                    Text(text = "VMP App Hidup 🔥")
+                }
+            }
+        }
+    }
+}
